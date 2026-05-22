44ad5ed77090336f57b34c7ffe2a895146e98c6d
Alex <alexnuy+github@gmail.com>
2026-05-22 17:53:12 -0500

    Ficed misnamed parmater

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def go(config: DictConfig):
                 f"{config['main']['components_repository']}/train_val_test_split",
                 'main',
                 parameters = {
-                    "input_artifact": "clean_sample.csv:latest",
+                    "input": "clean_sample.csv:latest",
                     "test_size": config["modeling"]["test_size"],
                     "random_seed": config["modeling"]["random_seed"],
                     "stratify_by": config["modeling"]["stratify_by"]
